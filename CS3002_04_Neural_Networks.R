a70191ba57cfcfa723fce2b5903d1dd011991531
FrancisDavison <francis.nj.davison@gmail.com>
2021-11-23 23:05:40 +0000

    added working directory autoselect code

--- CS3002_04_Neural_Networks.R
+++ CS3002_04_Neural_Networks.R
@@ -1,1 +1,11 @@
 #CS3002_04_Neural_Networks
+if((Sys.info()["nodename"])=="JANEWAY")
+{
+  setwd("C:\\Users\\Janeway\\Dropbox\\Github\\CS3002_03_Classification")
+  print("Working Directory set to C:\\Users\\Janeway\\Dropbox\\Github\\CS3002_04_Neural_Networks")
+}
+if((Sys.info()["nodename"])=="PICARD")
+{
+  setwd("C:\\Users\\Picard\\Dropbox\\Github\\CS3002_03_Classification")
+  print("Working directory set to C:\\Users\\Picard\\Dropbox\\Github\\CS3002_04_Neural_Networks")
+}
