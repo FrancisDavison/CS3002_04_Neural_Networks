1bdf27bca48b000dc340d8e862d0f8b686cd2add
FrancisDavison <francis.nj.davison@gmail.com>
2021-11-27 14:42:07 +0000

    fixed everything that Rstudio broke, because its a shit piece of barely functional software

--- CS3002_04_Neural_Networks_XOR.R
+++ CS3002_04_Neural_Networks_XOR.R
@@ -1,4 +1,14 @@
 #CS3002_04_Neural_Networks_Instructions
+if((Sys.info()["nodename"])=="JANEWAY")
+{
+  setwd("C:\\Users\\Janeway\\Dropbox\\Github\\CS3002_CS3002_04_Neural_Networks")
+  print("Working Directory set to C:\\Users\\Janeway\\Dropbox\\Github\\CS3002_04_Neural_Networks")
+}
+if((Sys.info()["nodename"])=="PICARD")
+{
+  setwd("C:\\Users\\Picard\\Dropbox\\Github\\CS3002_CS3002_04_Neural_Networks")
+  print("Working directory set to C:\\Users\\Picard\\Dropbox\\Github\\CS3002_04_Neural_Networks")
+}
 
 #install.packages("neuralnet")
 library(neuralnet)
